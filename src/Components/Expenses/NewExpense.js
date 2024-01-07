75d5218a93bd343d9f7867ab5aa1056a84e8f857
atishoff012803 <kumarsahuatish280301@gmail.com>
2024-01-07 23:18:26 +0530

    Commit-02
    Cancel Button Operation & Add Expense Button Feature.

--- src/Components/Expenses/NewExpense.js
+++ src/Components/Expenses/NewExpense.js
@@ -1,10 +1,24 @@
-import React from 'react';
+import React, { useState} from 'react';
 import '../ComponentCss/NewExpense.css';
 import ExpenseForm from './ExpenseForm';
 const NewExpense = (props) =>{
+    const [IsEditing, SetIsEditing] = useState(false);
+    const StartEditing = () => {    SetIsEditing(true);     }
+    const StopEditing = () => {     SetIsEditing(false);    }
     return(
         <div className="FormBox">
-                <ExpenseForm />
+                <div className="cnb">
+                    {
+                        !IsEditing &&
+                        <button className= "APBTN BTN1" onClick={StartEditing}>Add Expense</button>
+                    }
+                </div>
+                {
+                    IsEditing &&
+                    <ExpenseForm 
+                        onCancel = {StopEditing}
+                    />
+                }
         </div>
     );
 }
